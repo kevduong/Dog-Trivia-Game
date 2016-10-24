9c7611a5f2da91c4a30547de608fb6cf87bfcd9b
Kevin Duong <duong.kev@gmail.com>
2016-10-23 21:33:13 -0700

    timer isn't working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,145 @@
-
+var counter = 10;
 var correct = 0;
 var wrong = 0;
-var counter = 10;
+var currentPregunta = 0;
 
 var questions =      
     [{
-    prompt1: "The average weight of a German Shepherd:",
-    choices1: ["30-50lbs", "60-80lbs", "90-100lbs", "110-130lbs"],
-    answer: 2,
+    prompt: "The average weight of a German Shepherd is:",
+    choices: ["30-50lbs", "60-80lbs", "90-100lbs", "110-130lbs"],
+    answer: 1,
+    image: "assets/images/gsd.jpeg",
     },
     {
     prompt: "The Akita's origin is _____.",
     choices: ["South Korea", "Russia", "Australia", "Japan"],
     answer: 3,
      
-   }];    
+   }]; 
 
-$('.btn-default').on('click', countdownzz(), function(){
+
+$('#hello_world').hide();
+$('#timer').hide();
+
+/////////////////////   Setting the Timer //////////////////////////////
+function run(){
+    time = setInterval(decrement, 1000);
+}
+
+function decrement(){
+    counter--;
+    $('#timer').html('<h2> You have ' + counter + ' seconds left to answer</h2>');
+
+    if (counter == 0){
+        wrong++;
+        wrong();
+        $('#question').hide();
+    }
     
-    beginTest();
-    $('.btn-default').hide();
+function stopTime() {
+    counter = 10;
+};
+
+}
+
+
+//function finalCountDown() {
+//    counter--;
+//    $('#timer').html('<h2> You have ' + counter + ' seconds left to answer</h2>');
+//
+//    // Condition
+//    if (counter == 0){
+//        wrong++;
+//        wrong();
+//        $('#question').hide();
+//    }
+//};
+//  
+//function stopTime() {
+//    counter = 10;
+//};
+//
+//function timeGO() {
+//    counter = setInterval(finalCountDown, 900);
+//}
+//
+
+/////////////////////   Start    //////////////////////////////
+
+$('#start-btn').on('click', function(){
+    
+    beginTrivia();
+    
+    $('#start-btn').hide();
+    $('#hello_world').show();
+    run();
     
 });
 
-function beginTest() {
+function beginTrivia() {
     
-    $('.question').html(questions.prompt1);
-    $('.answer0').html(questions.choices1[0]);
-    $('.answer1').html(questions.choices1[1]);
-    $('.answer2').html(questions.choices1[2]);
-    $('.answer3').html(questions.choices1[3]);
+    $('#timer').show();
     
+    $('#question').html(questions[currentPregunta].prompt);
+    $('#question').append('<li value=0>'+ questions[currentPregunta].choices[0] + '</li>');
+    $('#question').append('<li value=1>'+ questions[currentPregunta].choices[1] + '</li>');
+    $('#question').append('<li value=2>'+ questions[currentPregunta].choices[2] + '</li>');
+    $('#question').append('<li value=3>'+ questions[currentPregunta].choices[3] + '</li>');
+    
+    //stats
+    
+    $('li').on('click', function(){
+        $('#question').hide();
+        $('#timer').hide();
+        stopTime();
+        if (this.value == questions[currentPregunta].answer){
+            correct++;
+            goodJob();
+        }
+        else {
+            wrong++;
+            wrong();
+        }
+    });
 };
 
-function countdownzz() {
-var countDown = setInterval(decrement, 1000);
 
-function decrement() {
-    
-    counter--;
-    $('#show-number').html('<h1>' + counter + '<h1>');
-    
-    if (counter === 0) {
-        console.log("blah");
-        wrong--;
-        stop();
-        //move to next question function
+/////////////////////   Correct    //////////////////////////////
+function goodJob() {
+    $('#answer').show();
+    $('#doggy').show();
+    $('#answer').html('<h3>That was PAWESOME. You must be a dog expert!</h3>');
+    $('#doggy').append('<img width="400px" src="' +questions[currentPregunta].image+ '">');
+    currentPregunta++;
+    timeOut();
+
+};
+
+
+/////////////////////   Wrong    //////////////////////////////
+function wrong() {
+    $('#answer').show();
+    $('#doggy').show();
+    $('#answer').html('<h3>That was ruff and pawful! Try harder!</h3>');
+    $('#doggy').append('<img width="400px" src="' +questions[currentPregunta].image+ '">');
+    currentPregunta++;
+    timeOut();
+};
+
+function timeOut() {
+    $('#timer').hide();
+    setTimeout(nextQuestion, 2000)
+};
+/////////////////////   NxtQuestion    //////////////////////////////
+function nextQuestion() {
+    if (currentPregunta < questions.length){
+        counter = 10;
+        beginTrivia();
+        $('#hello_world').show();
+        $('#answer').hide();
+        $('#doggy').hide();
+                           
     }
-}
-
-function stop() {
-    console.log("stop");
-    clearInterval(counter);
-    counter = 10;
-}
 };
+
+/////////////////////   Fin    //////////////////////////////
